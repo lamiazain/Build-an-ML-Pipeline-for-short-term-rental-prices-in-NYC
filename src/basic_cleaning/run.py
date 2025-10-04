1f994e1f21afce55176fe83b5e2032cd685581e9
Lamia <lamiahasan4@gmail.com>
2025-10-04 04:33:49 +0300

    Limiting longitude and latitude2

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,9 +29,7 @@ def go(args):
     artifact_path = wandb.use_artifact(args.input_artifact).file()
     df = pd.read_csv(artifact_path)
 
-    logger.info("Limiting Price column")
-    idx = df['price'].between(args.min_price, args.max_price)
-    df = df[idx].copy()
+
 
     logger.info("Limiting longitude and latitude")
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
